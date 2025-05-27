28415a146f76a44e28db0578545752994df4f841
ZhKuriabov <52123463+ZhKuriabov@users.noreply.github.com>
2025-05-27 18:43:49 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="test_forestry",
+    version="0.1.0",
+    description="",
+    author="Evgenii Kuriabov",
+    author_email="evgenii@example.com",
+    url="https://github.com/ZhKuriabov/test_rforestry",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "pandas",
+        # добавь другие зависимости, если нужно
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
