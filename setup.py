d3af4bb12e124aeb30d88dc52e173e6fb17f45b9
ZhKuriabov <52123463+ZhKuriabov@users.noreply.github.com>
2025-05-27 18:46:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="test_forestry",
-    version="0.1.0",
+    version="0.1.1",
     description="",
     author="Evgenii Kuriabov",
     author_email="evgenii@example.com",
